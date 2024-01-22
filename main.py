01c070ca0a308c0c0784e34c2a2cdc728887073e
damiankadziela81 <damiankadziela02@gmail.com>
2024-01-22 11:05:02 +0100

    Added yell option and text coloring

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import typer
 
 
 def print_hi(
+        yell: bool = typer.Option(False, help="Yell"),
         greet: str = typer.Option("Hi", help="Greeting"),
         name: str = typer.Option("John Doe", help="Enter name for greetings")):
     """ Greeting program """
-    print(f'{greet} {name}')
+    if yell:
+        typer.echo(typer.style(
+            f'{greet} {name} !!!',
+            fg=typer.colors.BRIGHT_WHITE,
+            bg=typer.colors.BRIGHT_RED
+        ))
+    else:
+        print(f'{greet} {name}')
 
 
 if __name__ == '__main__':
