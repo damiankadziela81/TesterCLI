2dfbf4abb6a79c41f797b50a1381359230aa03e3
damiankadziela81 <damiankadziela02@gmail.com>
2024-01-22 12:22:14 +0100

    Added method to generate random person profile in json with ability to save it to a file

--- cli/gen.py
+++ cli/gen.py
@@ -1,5 +1,8 @@
 import typer
 import pyperclip
+import json
+from datetime import datetime
+from pprint import pprint
 from functools import wraps
 from faker import Faker
 
@@ -33,6 +36,25 @@ def last_name():
 @clipboard
 def name():
     return faker.name()
+
+
+@app.command()
+def person(
+        save: bool = typer.Option(True, help="Save to file"),
+        filename: str = typer.Option("person.json", help="JSON filename")
+):
+    profile = faker.profile()
+    # convert DECIMAL in json, bc it's not serializable
+    profile['current_location'] = (str(profile['current_location'][0]),
+                                   str(profile['current_location'][1]))
+    # convert non serializable datetime.date format (needs datetime import)
+    profile['birthdate'] = profile['birthdate'].strftime("%Y.%m.%d")
+    pprint(profile)
+
+    # when run 'python main.py gen person --no-save' it will not create the file
+    if save:
+        with open(filename, 'w') as json_file:
+            json.dump(profile, json_file, indent=4)
 
 
 if __name__ == '__main__':
