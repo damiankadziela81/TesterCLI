fb57afae489398781aec77fe0f732b9c556fc59d
damiankadziela81 <damiankadziela02@gmail.com>
2024-01-22 10:53:46 +0100

    Changed arguments for options

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import typer
 
+# Run from terminal, use --help for more info
 
-def print_hi(name: str = typer.Argument("John Doe", help="Enter name for greetings")):
-    ''' Greeting program '''
-    print(f'Hi {name}')
+
+def print_hi(
+        greet: str = typer.Option("Hi", help="Greeting"),
+        name: str = typer.Option("John Doe", help="Enter name for greetings")):
+    """ Greeting program """
+    print(f'{greet} {name}')
 
 
 if __name__ == '__main__':
