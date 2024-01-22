12360560859c73093957469c4b3959372064039c
damiankadziela81 <damiankadziela02@gmail.com>
2024-01-22 11:48:01 +0100

    Added pyperclip to have the result available in the clipboard

--- cli/gen.py
+++ cli/gen.py
@@ -1,4 +1,5 @@
 import typer
+import pyperclip
 from faker import Faker
 
 app = typer.Typer()
@@ -7,17 +8,23 @@ faker = Faker()
 
 @app.command()
 def first_name():
-    typer.echo(faker.first_name())
+    random_first_name = faker.first_name()
+    typer.echo(random_first_name)
+    pyperclip.copy(random_first_name)
 
 
 @app.command()
 def last_name():
-    typer.echo(faker.last_name())
+    random_last_name = faker.last_name()
+    typer.echo(random_last_name)
+    pyperclip.copy(random_last_name)
 
 
 @app.command()
 def name():
-    typer.echo(faker.name())
+    random_name = faker.name()
+    typer.echo(random_name)
+    pyperclip.copy(random_name)
 
 
 if __name__ == '__main__':
